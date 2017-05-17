8be70b124c5eb4a5785afddf3681fe6f3892366c
DaveHsu <571460072@qq.com>
2017-05-17 19:51:00 +0800

    commit

--- petCommunity/src/main/java/com/lt/service/impl/CommodityServiceImpl.java
+++ petCommunity/src/main/java/com/lt/service/impl/CommodityServiceImpl.java
@@ -2,42 +2,45 @@ package com.lt.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lt.dao.ICommodityDao;
 import com.lt.pojo.Commodity;
 import com.lt.service.ICommodityService;
 
 @Service
 public class CommodityServiceImpl implements ICommodityService {
 
+	@Autowired
+	private ICommodityDao commodityDao;
+	
 	@Override
 	public List<Commodity> selectAllCommodities() {
-		// TODO Auto-generated method stub
-		return null;
+		return commodityDao.selectAllCommodities();
 	}
 
 	@Override
 	public Commodity selectCommodityById(Integer cid) {
-		// TODO Auto-generated method stub
-		return null;
+		return commodityDao.selectByPrimaryKey(cid);
 	}
 
 	@Override
 	public int insertCommodity(Commodity commodity) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = commodityDao.insert(commodity);
+		return i;
 	}
 
 	@Override
 	public int updateCommodity(Commodity commodity) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = commodityDao.updateByPrimaryKeySelective(commodity);
+		return i;
 	}
 
 	@Override
 	public int deleteCommodity(Integer cid) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = commodityDao.deleteByPrimaryKey(cid);
+		return i;
 	}
 
 }
--- petCommunity/src/main/java/com/lt/service/impl/PostServiceImpl.java
+++ petCommunity/src/main/java/com/lt/service/impl/PostServiceImpl.java
@@ -2,54 +2,56 @@ package com.lt.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lt.dao.IPostDao;
 import com.lt.pojo.Post;
 import com.lt.service.IPostService;
 
 @Service
 public class PostServiceImpl implements IPostService {
+	
+	@Autowired
+	private IPostDao postDao;
 
 	@Override
 	public List<Post> selectAllPosts() {
-		// TODO Auto-generated method stub
-		return null;
+		return postDao.selectAllPosts();
 	}
 
 	@Override
 	public Post selectPostsById(Integer pid) {
-		// TODO Auto-generated method stub
-		return null;
+		return postDao.selectByPrimaryKey(pid);
 	}
 
 	@Override
 	public List<Post> selectPostsByUser(Integer uid) {
-		// TODO Auto-generated method stub
-		return null;
+		return postDao.selectPostsByUser(uid);
 	}
 
 	@Override
 	public int insertPost(Post post) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = postDao.insert(post);
+		return i;
 	}
 
 	@Override
 	public int updatePost(Post post) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = postDao.updateByPrimaryKeySelective(post);
+		return i;
 	}
 
 	@Override
 	public int deletePost(Integer pid) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = postDao.deleteByUser(pid);
+		return i;
 	}
 
 	@Override
 	public int deletePostByUser(Integer uid) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = postDao.deleteByUser(uid);
+		return i;
 	}
 
 
