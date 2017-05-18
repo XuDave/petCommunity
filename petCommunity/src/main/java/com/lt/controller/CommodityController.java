0ac239bd7b912b8c9624f5b0abcfbcf230637b03
DaveHsu <571460072@qq.com>
2017-05-18 12:28:15 +0800

    commit

--- petCommunity/src/main/java/com/lt/controller/CommodityController.java
+++ petCommunity/src/main/java/com/lt/controller/CommodityController.java
@@ -1,10 +1,31 @@
 package com.lt.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lt.pojo.Commodity;
+import com.lt.service.ICommodityService;
 
 @Controller
 @RequestMapping("/commodity")
 public class CommodityController {
-
+   @Autowired
+   private ICommodityService commodityService;
+   
+   private Logger logger = Logger.getLogger(CommodityController.class);
+   
+   @RequestMapping("/commodityList")
+   public ModelAndView selectAllCommodities(ModelMap model){
+	   ModelAndView mv = new ModelAndView();
+	   List<Commodity> commodityList = commodityService.selectAllCommodities();
+	   model.addAttribute("commodityList", commodityList);
+	   mv.setViewName("commodity");
+	   return mv;
+   }
 }
--- petCommunity/src/main/java/com/lt/controller/PostController.java
+++ petCommunity/src/main/java/com/lt/controller/PostController.java
@@ -1,10 +1,33 @@
 package com.lt.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lt.pojo.Post;
+import com.lt.service.IPostService;
 
 @Controller
 @RequestMapping("/post")
 public class PostController {
+	
+	@Autowired
+	private IPostService postService;
+	
+	private Logger logger = Logger.getLogger(PostController.class);
+	
+	@RequestMapping("/postList")
+	public ModelAndView selectAllPosts(ModelMap model){
+		ModelAndView mv = new ModelAndView();
+		List<Post> postList = postService.selectAllPosts();
+		model.addAttribute("postList", postList);
+		mv.setViewName("post");
+		return mv;
+	}
 
 }
