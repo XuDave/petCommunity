bc7e875c4ab5b5c7a7fcf66164207d4592a29389
DaveHsu <571460072@qq.com>
2017-05-18 09:32:55 +0800

    commit

--- petCommunity/src/main/java/com/lt/service/ISecondReplyService.java
+++ petCommunity/src/main/java/com/lt/service/ISecondReplyService.java
@@ -1,5 +1,78 @@
 package com.lt.service;
 
-public interface ISecondReplyService {
+import java.util.List;
 
+import com.lt.pojo.SecondReply;
+
+public interface ISecondReplyService {
+   /**
+    * 查询全部的二级回复
+    * 
+    * @return
+    */
+	List<SecondReply> selectAllSecondReplies();
+	
+	/**
+	 * 根据主键查询二级回复
+	 * 
+	 * @param sId
+	 * @return
+	 */
+	SecondReply selectSecondReplyById(Integer sId);
+	
+	/**
+	 * 根据一级回复编号查询相应的二级回复
+	 * 
+	 * @param pId
+	 * @return
+	 */
+	List<SecondReply> selectSecondReplyByPostId(Integer pId);
+	
+	/**
+	 * 根据用户编号查询相应的二级回复
+	 * 
+	 * @param uId
+	 * @return
+	 */
+	List<SecondReply> selectSeconfReplyByUserId(Integer uId);
+	
+	/**
+	 * 添加二级回复
+	 * 
+	 * @param secondReply
+	 * @return
+	 */
+	int insertSecondReply(SecondReply secondReply);
+	
+	/**
+	 * 更新二级回复
+	 * 
+	 * @param secondReply
+	 * @return
+	 */
+	int updateSecondReply(SecondReply secondReply);
+	
+	/**
+	 * 根据主键删除二级回复
+	 * 
+	 * @param sId
+	 * @return
+	 */
+    int deleteSecondReply(Integer sId);
+    
+    /**
+     * 根据一级回复删除二级回复
+     * 
+     * @param fId
+     * @return
+     */
+    int deleteSecondReplyByFirstReply(Integer fId);
+    
+    /**
+     * 根据用户编号删除二级回复
+     * 
+     * @param uId
+     * @return
+     */
+    int deleteSecondReplyByUser(Integer uId);
 }
---
+++ petCommunity/src/main/java/com/lt/service/impl/SecondReplyServiceImpl.java
@@ -0,0 +1,64 @@
+package com.lt.service.impl;
+
+import java.util.List;
+
+import com.lt.pojo.SecondReply;
+import com.lt.service.ISecondReplyService;
+
+public class SecondReplyServiceImpl implements ISecondReplyService {
+
+	@Override
+	public List<SecondReply> selectAllSecondReplies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SecondReply selectSecondReplyById(Integer sId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SecondReply> selectSecondReplyByPostId(Integer pId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SecondReply> selectSeconfReplyByUserId(Integer uId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertSecondReply(SecondReply secondReply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateSecondReply(SecondReply secondReply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteSecondReply(Integer sId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteSecondReplyByFirstReply(Integer fId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteSecondReplyByUser(Integer uId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
